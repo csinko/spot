859d5d09436a3916522ce546e5ecb28bfebc0321
Cameron Sinko <csinko@gmail.com>
2017-11-05 11:13:57 -0500

    Working weather API

--- App.js
+++ App.js
@@ -1,92 +1,80 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import axios from 'axios'
+import { EventEmitter } from 'fbemitter';
 
 const API_Key = "HackPSU2017";
-const cityKey = "330119";
+
+    var emitter = new EventEmitter();
 
 export default class App extends React.Component {
 
   constructor(){
     super();
+    this.getWeather=this.getWeather.bind(this);
     this.state = {
-      
-      City : cityKey,
-      Days : [],
-
+        events: new Array()
     }
   }
 
-  getZipCode(address) {
-    
-    var zipCode;
-    addressAlt = address.replace(/ /g,'');
-    const API_Key = "AIzaSyA-AfLMIf-k0l4IEmktj4Egjz74AN-gQ2I";
-    const weatherAPIKey = "HackPSU2017";
-    const requestURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + addressAlt + "&key=" + API_Key;
+    componentDidMount() {
+        emitter.addListener('event', evt => {
+                this.setState({
+                    events: [].concat(evt)
+                });
+            });
+        this.getWeather([{
+            name: "Event 1",
+            date: "2017-11-07T15:00:00-05:00",
+            description: "My Event",
+            location: "293 E Exchange St, Akron, OH 44304, USA"
+        }]);
 
-    axios.get(requestURL)
-    .then( (response) => {
+    }
 
-      if(response.status == 200){
-        zipCode = response.data.results[0].address_components[6].long_name; 
-        return zipCode;
-      }
-      
-    })
-      .catch((error) => {
-        console.error(error);
 
-  });
-
-  }
-
-  getCityKey(zipCode){
-    console.log(zipCode);
-    console.log("http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=HackPSU2017&q=" + zipCode);
-    axios.get("http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=HackPSU2017&q=" + zipCode)
-    .then( (response) => {
-
-      if(response.status == 200){
+      getWeather(events) {
+          console.log("Getting Weather");
         
-       // weather = response.data.DailyForecasts.Day.IconPhrase;
-        console.log(zipCode);
-        console.log(response.data);
-
+          events.map(function(evt) {
+              address = evt.location.replace(/ /g, '');
+              const API_KEY = "AIzaSyA-AfLMIf-k0l4IEmktj4Egjz74AN-gQ2I";
+              fetch('https://maps.googleapis.com/maps/api/geocode/json?address='+ address + '&key=' + API_KEY)
+              .then((response) => response.json())
+              .then((responseJson) => {
+                  let zipCode = responseJson.results[0].address_components[7].long_name; 
+                  console.log(zipCode);
+                  fetch('http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=HackPSU2017&q=' + zipCode)
+                  .then((response2) => response2.json())
+                  .then((responseJson2) => {
+                      let cityKey = responseJson2[0].ParentCity.Key;
+                      fetch('http://dataservice.accuweather.com/forecasts/v1/daily/15day/' + cityKey + '?apikey=HackPSU2017')
+                      .then((response3) => response3.json())
+                      .then((responseJson3) => {
+                          console.log(responseJson3);
+                          let dayWeather = responseJson3.DailyForecasts[0];
+                          evt.weather = dayWeather.Day.IconPhrase;
+                          evt.high = dayWeather.Temperature.Maximum.Value;
+                          evt.low = dayWeather.Temperature.Minimum.Value;
+                          emitter.emit('event', evt);
+                          
+                      })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+                  })
+                  .catch((error) => {
+                      console.error(error);
+                  });
+              })
+              .catch((error) => {
+                  console.error(error);
+              });
+            
+          });
       }
-      
-    })
-      .catch((error) => {
-        console.error(error);
 
-  });
-  }
-
-  getForecast(cityKey){
-
-    const requestURL = "http://dataservice.accuweather.com/forecasts/v1/daily/15day/" + cityKey + "?apikey=" + API_Key;
-    axios.get(requestURL)
-    .then( (response) => {
-
-      if(response.status == 200){
-        
-       // weather = response.data.DailyForecasts.Day.IconPhrase;
-        console.log(response.data.DailyForecasts[0].Day.IconPhrase);
-
-      }
-      
-    })
-      .catch((error) => {
-        console.error(error);
-
-  });
-}
   render() {
-    if(this.state.Days <= 0){
-
-      console.log(this.getZipCode("2181 Duxbury Drive"));
-
-    }
+      console.log(this.state.events);
     
     return (
       <View style={styles.container}>
